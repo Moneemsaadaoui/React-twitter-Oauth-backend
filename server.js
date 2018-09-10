61a0f4c9d3847ecf52f826f7b0ed54159b5823d8
unknown <abdelmoneem.saadaoui@satoripop.com>
2018-09-10 16:27:40 +0100

    make it better

--- server.js
+++ server.js
@@ -173,7 +173,7 @@ router.route("/auth/me").get(authenticate, getCurrentUser, getOne);
 
 app.use("/api/v1", router);
 
-app.listen(4000);
+app.listen(80);
 module.exports = app;
 
 console.log("Server running at http://localhost:4000/");
